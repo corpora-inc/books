aa2d7cb5677056c8b77d9298c3022c60072331ba
Kevin Bravo <bravokevinto@gmail.com>
2025-06-03 22:55:06 -0400

    feat: (corpus-reader) enhace header

--- corpus-reader/src/components/HomeHeader.tsx
+++ corpus-reader/src/components/HomeHeader.tsx
@@ -1,8 +1,9 @@
-import { BookOpenIcon, ClockIcon, HomeIcon, PlusIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react";
 import { Button } from "./ui/button";
 
 import BookSearchDialog from "./BookSearchDialog";
 import { BookEntry } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const HomeHeader = ({
   books,
@@ -12,37 +13,36 @@ const HomeHeader = ({
   handleAddBookToLibrary: () => Promise<void>;
 }) => {
   return (
-    <div className="bg-background/60 backdrop-blur-sm border-b sticky top-0 z-50">
-      <div className="flex h-16 items-center justify-between px-4">
-        <div className="flex items-center md:hidden">
-          <BookOpenIcon className="h-6 w-6 text-primary mr-2" />
-          <h1 className="text-xl font-bold">Corpus Reader</h1>
+    <div className=" bg-background/60 backdrop-blur-sm border-b sticky top-0 z-50">
+      <div className="flex h-16 items-center justify-between  container px-4 mx-auto max-w-7xl">
+        <div className="flex items-center ">
+          <Avatar>
+            <AvatarImage src="/logo.png" />
+            <AvatarFallback>Logo</AvatarFallback>
+          </Avatar>
+          <h1 className="text-xl font-bold">Corporium</h1>
         </div>
-
         <div className="hidden md:block">
-          <Button variant="ghost" size="sm" className="mr-1">
-            <HomeIcon className="h-4 w-4 mr-2" />
-            Home
-          </Button>
-          <Button variant="ghost" size="sm" className="mr-1">
-            <BookOpenIcon className="h-4 w-4 mr-2" />
-            Library
-          </Button>
-          <Button variant="ghost" size="sm">
-            <ClockIcon className="h-4 w-4 mr-2" />
-            Recent
-          </Button>
+          {/* TODO: add bookmarks and notes section */}
         </div>
         <div className="flex items-center space-x-2">
           {/* Search Dialog with debounce */}
           <BookSearchDialog books={books} />
           <Button
-            variant="ghost"
             size="icon"
             className="h-8 w-8 md:hidden"
             onClick={handleAddBookToLibrary}
           >
             <PlusIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            className="hidden md:block"
+            onClick={handleAddBookToLibrary}
+            size="sm"
+          >
+            <p>
+              <PlusIcon className="inline" /> Add book
+            </p>
           </Button>
         </div>
       </div>
